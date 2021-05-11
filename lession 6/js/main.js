bbb047c262dd8108af4ecf4a0c3252867d590826
Mike <headmn@gmail.com>
2021-05-11 21:27:03 +0300

    hw_6

--- lession 6/js/main.js
+++ lession 6/js/main.js
@@ -3,7 +3,6 @@ const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-a
 const app = new Vue({
     el: '#app',
     data: {
-        searchLine: '',
     },
     methods: {
         getJson(url){
